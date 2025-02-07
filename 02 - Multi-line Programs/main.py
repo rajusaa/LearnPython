31e5fb4accc2d6e10a6beaef485b94bd27d2af8e
rajusaa <santoshkumar.skr1@gmail.com>
2025-02-07 09:09:36 +0800

    multi line programs

---
+++ 02 - Multi-line Programs/main.py
@@ -0,0 +1,11 @@
+print("Hello, World!")
+
+#print() function will leave an empty line
+print()
+
+# \n is used to signal python to start a new line
+print("This is the first line. \nThis is the second line.")
+
+# three quotation marks can also print multiple lines
+print('''Three quotes can be used also
+to print multiple lines using single print function''')
